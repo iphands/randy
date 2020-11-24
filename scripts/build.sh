5e69501ab5f7145a6e152904e68d3b85c6d42eac
Ian Page Hands <iphands@gmail.com>
2020-11-24 11:28:35 -0500

    build differently on laptop

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 set -e
 
-RUSTFLAGS="-C opt-level=3 -C debuginfo=0 -C target-cpu=native" cargo build --release --features timings
+if [ "$HOSTNAME" == "handslap" ]
+then
+    RUSTFLAGS="-C opt-level=3 -C debuginfo=0 -C target-cpu=native" cargo build --release --no-default-features --features timings
+else
+    RUSTFLAGS="-C opt-level=3 -C debuginfo=0 -C target-cpu=native" cargo build --release --features timings
+fi
+
 cp ./target/release/randy ./target/release/randy.nostrip
 strip -s ./target/release/randy
 ls -lh ./target/release/randy ./target/release/randy.nostrip
