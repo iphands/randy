f5368fe5d1a65189b283604fce8bcf8c17e910bc
Ian Page Hands <iphands@gmail.com>
2020-11-25 05:46:19 -0500

    toying with build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 set -e
 
+export RUSTFLAGS="-C opt-level=3 -C debuginfo=0 -C target-cpu=native"
+
 if [ "$HOSTNAME" == "handslap" ]
 then
-    RUSTFLAGS="-C opt-level=3 -C debuginfo=0 -C target-cpu=native" cargo build --release --no-default-features
-else
-    RUSTFLAGS="-C opt-level=3 -C debuginfo=0 -C target-cpu=native" cargo build --release --features timings
+    cargo build --release --no-default-features --features timings
     cp ./target/release/randy ./target/release/randy.timings
-    RUSTFLAGS="-C opt-level=3 -C debuginfo=0 -C target-cpu=native" cargo build --release
+    cargo build --release --no-default-features
+else
+    cargo build --release --features timings
+    cp ./target/release/randy ./target/release/randy.timings
+    cargo build --release
 fi
 
 cp ./target/release/randy ./target/release/randy.nostrip
 strip -s ./target/release/randy
-ls -lh ./target/release/randy ./target/release/randy.nostrip
+
+cp ./target/release/randy.timings ./target/release/randy.timings.nostrip
+strip -s ./target/release/randy.timings
+
+ls -lh ./target/release/randy \
+   ./target/release/randy.nostrip \
+   ./target/release/randy.timings \
+   ./target/release/randy.timings.nostrip \
