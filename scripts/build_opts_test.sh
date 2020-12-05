d3081abd92534dea3dd8c9b4efc872890cdc7a9a
Ian Page Hands <iphands@gmail.com>
2020-12-05 09:59:20 -0500

    switching license_file to license

--- scripts/build_opts_test.sh
+++ scripts/build_opts_test.sh
@@ -2,12 +2,31 @@
 
 mkdir -p ./stash/test_bins/
 
-FEATURES=(avx avx2 sse sse2 sse3 sse4a ssse3)
+# FEATURES=(avx avx2 sse sse2 sse3 sse4a ssse3)
+# for feature in ${FEATURES[@]}
+# do
+#     echo "TARGET FEATURE: $feature"
+#     FULL="+${feature}"
+#     for off in ${FEATURES[@]}
+#     do
+#         if [ `[ "$off" != "$feature" ] ; echo $?` == 0 ]
+#         then
+#             FULL="$FULL,-${off}"
+#         fi
+#     done
+#     echo "DOING $FULL"
+#     export RUSTFLAGS="-C opt-level=3 -C debuginfo=0 -C target-cpu=native -C target-feature=$FULL"
+#     cargo build --release --features timings,nvidia,sensors
+#     strip -s ./target/release/randy
+#     mv ./target/release/randy ./stash/test_bins/randy_${feature}
+# done
 
+
+FEATURES=(avx avx2 sse3 sse4a ssse3)
 for feature in ${FEATURES[@]}
 do
     echo "TARGET FEATURE: $feature"
-    FULL="+${feature}"
+    FULL="+sse,+sse2,+${feature}"
 
     for off in ${FEATURES[@]}
     do
@@ -19,7 +38,7 @@ do
 
     echo "DOING $FULL"
     export RUSTFLAGS="-C opt-level=3 -C debuginfo=0 -C target-cpu=native -C target-feature=$FULL"
-    cargo build --release --features timings,nvidia,sensors
+    cargo build --release --features timings,sensors
     strip -s ./target/release/randy
     mv ./target/release/randy ./stash/test_bins/randy_${feature}
 done
